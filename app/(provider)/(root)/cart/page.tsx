067259cea14afd32f5b1227fa8d8c6b81c7f3509
pikavlzk <t01045238401@gmail.com>
2024-10-01 15:07:29 +0900

    make Cart(UI)

--- app/(provider)/(root)/cart/page.tsx
+++ app/(provider)/(root)/cart/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function CartPage() {
   return (
-    <div>page</div>
-  )
+    <main className="text-center">
+      <h2 className="font-bold text-4xl mt-10">장바구니</h2>
+      <p className="mt-10">장바구니가 비어 있습니다.</p>
+
+      <Link href={"/"}><button className="border mt-10 w-64 h-14 b border-black font-semibold hover:-translate-y-1 transition-all">쇼핑하러 가기</button></Link>
+    </main>
+  );
 }
 
-export default CartPage
+export default CartPage;
